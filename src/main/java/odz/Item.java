4446d6d8da4148c7ea2f7c6ee81c8bd6d86bcd4d
Oljk <oljk2512@gmail.com>
2019-12-23 00:09:56 +0200

    extract Item class

---
+++ src/main/java/odz/Item.java
@@ -0,0 +1,92 @@
+package odz;
+
+public class Item {
+    private String title;
+    private double price;
+    private int quantity;
+    private ItemType type;
+
+    public enum ItemType { NEW, REGULAR, SECOND_FREE, SALE }
+
+    public Item(String title, double price, int quantity, ItemType type) {
+        setTitle(title);
+        setPrice(price);
+        setQuantity(quantity);
+        setType(type);
+        checkItem();
+    }
+
+    /**
+     * Calculates item's discount.
+     * For NEW item discount is 0%;
+     * For SECOND_FREE item discount is 50% if quantity > 1
+     * For SALE item discount is 70%
+     * For each full 10 not NEW items item gets additional 1% discount,
+     * but not more than 80% total
+     */
+    public int calculateDiscount() {
+        int discount = 0;
+        switch (type) {
+            case NEW:
+                return 0;
+            case REGULAR:
+                discount = 0;
+                break;
+            case SECOND_FREE:
+                if (quantity > 1)
+                    discount = 50;
+                break;
+            case SALE:
+                discount = 70;
+                break;
+        }
+        discount += quantity / 10;
+        if (discount > 80)
+            discount = 80;
+        return discount;
+    }
+
+    /**
+     * Checks if item fields have correct values
+     */
+    private void checkItem() {
+        if (title == null || title.length() == 0 || title.length() > 32)
+            throw new IllegalArgumentException("Illegal title");
+        if (price < 0.01)
+            throw new IllegalArgumentException("Illegal price");
+        if (quantity <= 0)
+            throw new IllegalArgumentException("Illegal quantity");
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public ItemType getType() {
+        return type;
+    }
+
+    public void setType(ItemType type) {
+        this.type = type;
+    }
+}
